75efc0281432181857aa1c8862a95214cd14f08c
Maxime Piraux <maxime.piraux@student.uclouvain.be>
2018-04-03 18:39:45 +0200

    Makes received transport parameters available

--- pigotls.go
+++ pigotls.go
@@ -4,9 +4,30 @@ package pigotls
 #cgo LDFLAGS: ${SRCDIR}/picotls/libpicotls-core.a ${SRCDIR}/picotls/libpicotls-openssl.a -lssl -lcrypto
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
 #include <picotls/include/picotls/openssl.h>
 #include <picotls/include/picotls.h>
 
+#define QUIC_TP_EXTENSION  26
+
+int collect_quic_extension(ptls_t *tls, struct st_ptls_handshake_properties_t *properties, uint16_t type) {
+	return type == QUIC_TP_EXTENSION;  // Only collect QUIC extensions
+}
+
+
+int collected_extensions(ptls_t *tls, struct st_ptls_handshake_properties_t *properties, ptls_raw_extension_t *extensions) {
+	while(extensions->type != 0xffff) {
+		if(extensions->type == QUIC_TP_EXTENSION) {
+			ptls_raw_extension_t *rec_ext = properties->additional_extensions + 1;
+			rec_ext->type = extensions->type;
+			rec_ext->data.base = (uint8_t*)malloc(extensions->data.len);
+			rec_ext->data.len = extensions->data.len;
+			memcpy(rec_ext->data.base, extensions->data.base, extensions->data.len);
+			break;
+		}
+	}
+	return 0;
+}
 
 void init_ctx(ptls_context_t *ctx) {
 	ctx->random_bytes = ptls_openssl_random_bytes;
@@ -18,6 +39,8 @@ void init_ctx(ptls_context_t *ctx) {
 void set_handshake_properties(ptls_handshake_properties_t *props, ptls_iovec_t *alpn) {
 	props->client.negotiated_protocols.count = 1;
 	props->client.negotiated_protocols.list = alpn;
+	props->collect_extension = collect_quic_extension;
+	props->collected_extensions = collected_extensions;
 }
 
 void set_extension_data(ptls_handshake_properties_t *props, uint16_t type, ptls_iovec_t data) {
@@ -30,6 +53,10 @@ void set_extension_data(ptls_handshake_properties_t *props, uint16_t type, ptls_
 	extensions[1].data.len = 0;
 
 	props->additional_extensions = extensions;
+}
+
+ptls_raw_extension_t get_extension_data(ptls_handshake_properties_t *props, int index) {
+	return props->additional_extensions[index];
 }
 */
 import "C"
@@ -74,6 +101,13 @@ func (c Context) InitializeCertificateVerifier() {
 func (c Context) SetQUICTransportParameters(extensionData []byte) {
 	C.set_extension_data(c.handshakeProperties, QuicTransportParametersTLSExtension, toIOVec(extensionData))
 }
+func (c Context) GetReceivedQUICTransportParameters() []byte {
+	extension := C.get_extension_data(c.handshakeProperties, 1)
+	if extension._type == QuicTransportParametersTLSExtension {
+		return ioVecToSlice(extension.data)
+	}
+	return nil
+}
 
 type Connection struct {
 	Context
@@ -111,14 +145,17 @@ func (c *Connection) Handshake(data []byte) ([]byte, bool, error) {
 		return nil, false, Error{int(ret)}
 	}
 
-	return toSlice(sendbuf), ret != 0, nil
+	return bufToSlice(sendbuf), ret != 0, nil
 }
 func (c *Connection) Close() {
 	C.ptls_free(c.tls)
 }
 
-func toSlice(buf C.ptls_buffer_t) []byte {
+func bufToSlice(buf C.ptls_buffer_t) []byte {
 	return C.GoBytes(unsafe.Pointer(buf.base), C.int(buf.off))
+}
+func ioVecToSlice(vec C.ptls_iovec_t) []byte {
+	return C.GoBytes(unsafe.Pointer(vec.base), C.int(vec.len))
 }
 func toIOVec(data []byte) C.ptls_iovec_t {
 	return C.ptls_iovec_init(C.CBytes(data), sizeofBytes(data))
