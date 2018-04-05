cc3d39891b147ced709f57173fa3a490ddd6f40d
Maxime Piraux <maxime.piraux@student.uclouvain.be>
2018-04-05 15:05:34 +0200

    Implements bindings for 0-RTT

--- pigotls.go
+++ pigotls.go
@@ -35,11 +35,14 @@ void init_ctx(ptls_context_t *ctx) {
 	ctx->get_time = &ptls_get_time;
 }
 
-void set_handshake_properties(ptls_handshake_properties_t *props, ptls_iovec_t *alpn) {
+void set_handshake_properties(ptls_handshake_properties_t *props, ptls_iovec_t *alpn, ptls_iovec_t *session_ticket) {
 	props->client.negotiated_protocols.count = 1;
 	props->client.negotiated_protocols.list = alpn;
 	props->collect_extension = collect_quic_extension;
 	props->collected_extensions = collected_extensions;
+	if (session_ticket != NULL && session_ticket->base != NULL) {
+		props->client.session_ticket = *session_ticket;
+	}
 }
 
 void set_extension_data(ptls_handshake_properties_t *props, uint16_t type, ptls_iovec_t data) {
@@ -56,6 +59,22 @@ void set_extension_data(ptls_handshake_properties_t *props, uint16_t type, ptls_
 
 ptls_raw_extension_t get_extension_data(ptls_handshake_properties_t *props, int index) {
 	return props->additional_extensions[index];
+}
+
+int cb(ptls_save_ticket_t *self, ptls_t *tls, ptls_iovec_t src) {
+	ptls_iovec_t *receiver = *(ptls_iovec_t**) (((char*)self) + sizeof(ptls_save_ticket_t));
+	receiver->base = malloc(src.len);
+	memcpy(receiver->base, src.base, src.len);
+	receiver->len = src.len;
+	return 0;
+}
+
+void set_ticket_cb(ptls_context_t *ctx, ptls_iovec_t *receiver) {
+	ptls_save_ticket_t* save_ticket = malloc(sizeof(ptls_save_ticket_t) + sizeof(ptls_iovec_t*));
+	save_ticket->cb = cb;
+	ptls_iovec_t** ppreceiver = (ptls_iovec_t**)(((char*)save_ticket) + sizeof(ptls_save_ticket_t));
+	*ppreceiver = receiver;
+	ctx->save_ticket = save_ticket;
 }
 */
 import "C"
@@ -80,17 +99,21 @@ func (e Error) Error() string {
 type Context struct {
 	ctx                 *C.ptls_context_t
 	handshakeProperties *C.ptls_handshake_properties_t
+	savedTicket 		*C.ptls_iovec_t
 }
 
-func NewContext(ALPN string) Context {
+func NewContext(ALPN string, resumptionTicket []byte) Context {
 	var ctx C.ptls_context_t
 	var handshakeProperties C.ptls_handshake_properties_t
-	c := Context{&ctx, &handshakeProperties}
+	var savedTicket C.ptls_iovec_t
+	c := Context{&ctx, &handshakeProperties, &savedTicket}
 
 	C.init_ctx(&ctx)
 
 	alpnVec := toIOVec([]byte(ALPN))
-	C.set_handshake_properties(c.handshakeProperties, &alpnVec)
+	resumptionTicketVec := toIOVec(resumptionTicket)
+	C.set_handshake_properties(c.handshakeProperties, &alpnVec, &resumptionTicketVec)
+	C.set_ticket_cb(c.ctx, c.savedTicket)
 
 	return c
 }
@@ -109,38 +132,56 @@ func (c Context) GetReceivedQUICTransportParameters() []byte {
 	}
 	return nil
 }
-
+func (c Context) GetResumptionTicket() []byte {
+	return ioVecToSlice(*c.savedTicket)
+}
 type Connection struct {
 	Context
 	tls *C.ptls_t
+	closed bool
 }
 
-func NewConnection(serverName string, ALPN string) *Connection {
+func NewConnection(serverName string, ALPN string, resumptionTicket []byte) *Connection {
 	c := new(Connection)
-	c.Context = NewContext(ALPN)
+	c.Context = NewContext(ALPN, resumptionTicket)
 	c.tls = C.ptls_new(c.Context.ctx, 0)
 	C.ptls_set_server_name(c.tls, C.CString(serverName), sizeofString(serverName))
 	return c
 }
 func (c *Connection) InitiateHandshake() ([]byte, bool, error) {
-	return c.Handshake(nil)
-}
-
-// Uses data received and returns data to be sent to the peer as well as a boolean indicating if the handshake should continue
-func (c *Connection) Handshake(data []byte) ([]byte, bool, error) {
 	var sendbuf C.ptls_buffer_t
 	C.ptls_buffer_init(&sendbuf, unsafe.Pointer(C.CString("")), 0)
 	defer C.ptls_buffer_dispose(&sendbuf)
 
-	var ret C.int
-	if data == nil {
-		ret = C.ptls_handshake(c.tls, &sendbuf, nil, nil, c.Context.handshakeProperties)
-	} else {
-		recbuf := C.CBytes(data)
-		defer C.free(recbuf)
-		rec_size := sizeofBytes(data)
+	ret := C.int(0)
+	ret = C.ptls_handshake(c.tls, &sendbuf, nil, nil, c.Context.handshakeProperties)
+	if ret != 0 && ret != C.PTLS_ERROR_IN_PROGRESS {
+		return nil, false, Error{int(ret)}
+	}
 
-		ret = C.ptls_handshake(c.tls, &sendbuf, recbuf, &rec_size, c.Context.handshakeProperties)
+	return bufToSlice(sendbuf), ret != 0, nil
+}
+
+// Uses data received and returns data to be sent to the peer as well as a boolean indicating if the handshake should continue
+func (c *Connection) Input(data []byte) ([]byte, bool, error) {
+	var sendbuf C.ptls_buffer_t
+	C.ptls_buffer_init(&sendbuf, unsafe.Pointer(C.CString("")), 0)
+	defer C.ptls_buffer_dispose(&sendbuf)
+
+	roff := (C.size_t)(0)
+	inputLen := (C.size_t)(0)
+	ret := C.int(0)
+	for (roff < (C.size_t)(len(data))) && (ret == 0 || ret == C.PTLS_ERROR_IN_PROGRESS) {
+		recbuf := C.CBytes(data[roff:])
+		defer C.free(recbuf)
+		inputLen = sizeofBytes(data[roff:])
+
+		if C.ptls_handshake_is_complete(c.tls) == 0 {
+			ret = C.ptls_handshake(c.tls, &sendbuf, recbuf, &inputLen, c.Context.handshakeProperties)
+		} else {
+			ret = C.ptls_receive(c.tls, &sendbuf, recbuf, &inputLen)
+		}
+		roff += inputLen
 	}
 
 	if ret != 0 && ret != C.PTLS_ERROR_IN_PROGRESS {
@@ -205,11 +246,14 @@ func (c *Connection) ExportSecret(label string, context []byte, isEarly bool) ([
 	return output[:c.HashDigestSize()], nil
 }
 func (c *Connection) Close() {
-	C.ptls_free(c.tls)
+	if !c.closed {
+		C.ptls_free(c.tls)
+		c.closed = true
+	}
 }
 
 func bufToSlice(buf C.ptls_buffer_t) []byte  { return C.GoBytes(unsafe.Pointer(buf.base), C.int(buf.off)) }
 func ioVecToSlice(vec C.ptls_iovec_t) []byte { return C.GoBytes(unsafe.Pointer(vec.base), C.int(vec.len)) }
-func toIOVec(data []byte) C.ptls_iovec_t     { return C.ptls_iovec_init(C.CBytes(data), sizeofBytes(data)) }
+func toIOVec(data []byte) C.ptls_iovec_t     { if data != nil { return C.ptls_iovec_init(C.CBytes(data), sizeofBytes(data)) }; return C.ptls_iovec_init(nil, 0) }
 func sizeofString(s string) C.size_t         { return (C.size_t)(len(s)) }
 func sizeofBytes(b []byte) C.size_t          { return (C.size_t)(len(b)) }
